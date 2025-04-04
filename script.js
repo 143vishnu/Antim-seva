b1158b0b8962570e18203e6491eeb63691aa17d4
vishnuvardhan orsu <131022963+143vishnu@users.noreply.github.com>
2025-04-05 00:01:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -244,3 +244,18 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const video = document.getElementById("heroVideo");
+    const playPauseBtn = document.getElementById("playPauseBtn");
+
+    playPauseBtn.addEventListener("click", function () {
+        if (video.paused) {
+            video.play();
+            playPauseBtn.textContent = "Pause";
+        } else {
+            video.pause();
+            playPauseBtn.textContent = "Play";
+        }
+    });
+});
